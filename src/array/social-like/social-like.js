9869adc224dbeb6145de08bfad4b9dfb67b0b6c4
Orenikoe <orental21@gmail.com>
2022-10-19 10:18:17 +0100

    solved

--- src/array/social-like/social-like.js
+++ src/array/social-like/social-like.js
@@ -8,5 +8,15 @@ Create a function `getLikes` which receives an array of names, and returns:
 */
 
 // TODO add your code here
-
+function getLikes(array) {
+if (array.length === 0) {
+    return "Be the first to like this"
+} else if (array.length === 1) {
+    return `${array[0]} likes this`
+} else if (array.length === 2) {
+    return `${array[0]} and ${array[1]} like this`
+} else {
+    return `${array[0]} and ${array.length - 1} other people like this`
+}
+}
 module.exports = getLikes;
